ba219ae1ed3e5806355d67a98eccbfb19dcccd6a
dcook6 <derekjamescook@gmail.com>
2016-10-20 21:38:39 -0600

    Update to Finalized Revision
    Tested and Complete

--- Register_TB.v
+++ Register_TB.v
@@ -7,14 +7,13 @@
 *	Outputs: reg1data, reg2data
 *
 */
-`include "registers.v"
 module Reg_TB();
 
-	reg 		clk, rst, wp_;
+	reg 	    clk, rst, wp_;
 	reg[2:0]    rs1, rs2, rd;
 	reg[15:0]   data_in;
 	
-	wire[15:0] 	reg1data, reg2data; 
+	wire[15:0]  reg1data, reg2data; 
 
 	
 
@@ -23,16 +22,13 @@ module Reg_TB();
 
 
 	initial begin
-		$dumpfile("Reg_TB.vcd");
-		$dumpvars(0, Reg_TB);
-		clk = 0;	error_count = 0;	rst = 0;
+		clk = 0;	rst = 0;
 
 		#5 rst = 1;
 		#10 rst = 0;	rs1 = 0; rs2 = 1;
 		#10 rst = 0;	rs1 = 1; rs2 = 0;
 		#10 rst = 0;	rs1 = 1; rs2 = 1;
-		#5 $display("Finished Register Test Bench. ");
+		#5 
 		$finish;
-
 	end
 endmodule
