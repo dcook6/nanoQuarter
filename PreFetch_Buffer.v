227670bc30e9fe2e276714de7c7fad5c09f7472a
chinnnathan <chinnnathan@gmail.com>
2016-10-22 11:53:15 -0600

    Update PreFetch_Buffer.v

--- PreFetch_Buffer.v
+++ PreFetch_Buffer.v
@@ -24,10 +24,10 @@ module PrefetchBuffer(	input 			clk, 	// System Clock
 	begin
 		if(wp_ === 1)
 		begin 
-			inst		= inst1; //Greg said these need to be non-blocking <=
-			inst_mem 	= inst2; //Greg said these need to be non-blocking <=
+			inst		<= inst1;
+			inst_mem 	<= inst2;
 		end
 		else
-			inst	= inst_mem; //Greg said these need to be non-blocking <=
+			inst	<= inst_mem;
 	end
 endmodule
