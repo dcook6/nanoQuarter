a8ac36bb944f3adfd024bcc7a12a82b6af26302c
chinnnathan <chinnnathan@gmail.com>
2016-10-22 11:49:42 -0600

    Update PC.v
    non-blocking statements

--- PC.v
+++ PC.v
@@ -18,8 +18,8 @@ module PC( input 		clk, // System Clock
 	always @ (posedge clk or posedge rst) //needs non blocking <=
 	 begin
 		if (rst == 1)
-			PC_out = 0;
+			PC_out <= 0;
 		else
-			PC_out = new_PC;
+			PC_out <= new_PC;
 	 end
 endmodule
